c97dd18c79e294f62efd0f1963fa30cc3e0989f9
Lorraine <lorraineconnellyzero@gmail.com>
2022-07-01 11:52:16 +0800

    Created composite TaskList component composed of Task components.

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+
   return (
     <div className={`list-item ${state}`}>
       <label
